4aa152e214c14d063b1991330d2da5094927d025
MatisDerrico <matisderrico@hotmail.com>
2024-06-26 12:59:32 +0200

    balise meta keywords

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta name="keywords" content="salon de coiffure, barbershop, salon de tatouage, coupe de cheveux, soins capillaires, coiffure homme, tatouages, soins de barbe, coupe enfant, barbe, coloration">
         <title inertia>{{ config('app.name', 'Mlle-derrico') }}</title>
 
         <!-- Fonts -->
